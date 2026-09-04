deb9e0ca625affe334426937b3507bfc64697fe9
Arpit yadav <115612258+Arpityadavvvv@users.noreply.github.com>
2026-09-05 01:14:05 +0530

    started learning python today
    Added examples of string and numerical variable basics in Python.

--- 02-Python /basics.py
+++ 02-Python /basics.py
@@ -1,1 +1,29 @@
+# class 01 python basics ( varibles and its types)          4 sept 2026
 
+# --- String Variable Basics ---
+
+name = "shree ganeshay namah"
+
+# Just by writing name of variable and running it, it will give you the content inside
+name 
+
+# Type will tell us type of variable right now
+type(name) 
+
+print(name)
+
+
+# --- Numerical Variables & Operations ---
+
+pizza = 100
+samosa = 20
+slize = 30.01
+
+total = pizza + samosa + slize
+
+total
+
+type(total)
+
+# This > operator means "is total greater than 200"
+total > 200
