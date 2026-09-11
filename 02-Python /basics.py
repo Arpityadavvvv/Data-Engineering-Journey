c31951c5d9e6dfcdb23f46ea179da5ad662bfc84
Arpit yadav <115612258+Arpityadavvvv@users.noreply.github.com>
2026-09-11 10:07:29 +0530

    Add Python basics with examples on numbers and strings

--- 02-Python /basics.py
+++ 02-Python /basics.py
@@ -27,3 +27,145 @@ type(total)
 
 # This > operator means "is total greater than 200"
 total > 200
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# class - 02 (numbers)
+base = 10
+height = 8
+area = 1/2 *base*height  
+area  # ouput = 40
+
+type(area)  # by this we will know the area 
+
+x=30
+y=7
+
+x+y = 37
+x*y = 210
+x/y = 4.285714285714286
+x//y = 4  # it is a integer divison 
+x**y = 21870000000 #x to the power y 
+
+sci = 1.2e+2 # 120
+sci2 = 1e-2  # 0.01 
+
+food = 3
+water = 5
+Total = food + water #8
+
+food = "3"
+water = "5"
+total = food + water  # but in this case it will give output "35" , now to tackle this problem 
+
+total = float(food) + float(water) # this is called type casting 
+
+# this is how we import this library 
+import math 
+x=16 
+math.sqrt(x)  # output is 4 
+
+format (5 ,'b')  # it will convert to binary 
+format (10 ,'b') # 1010
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# class -03 (strings)
+
+first = "Mohan"
+last = "sharma"
+
+name =first + " " + last  # WE CAN ADD LIKE THIS 
+#output = Mohan sharma 
+
+name = f"{first} {last}"  # this is also an method in python for adding a string , varible should be in {}
+
+# this is how indexing works 
+name[0] = m
+name[1] = o
+
+# slice operator
+name[0:4] # SLICE operator = it will give you sliced string out of this (o to 4)
+# "moha"
+
+name[6:12]  # 6 included and 12 exlcuded (6,7,8,9,10,11)
+# sharma 
+
+name[-2] # this in only in python , where -2 is saaying second element from last 
+
+len(name) # to get size of string 
+
+food_Story = " i am 'banana' "  # when we have to use single quotes , we will use double quotes in outer 
+s = ' they have "oops" moment ' # when we have to use double quotes , we will use single quotes in outer 
+
+
+recipi = ''' \n veg biryani with saffron , cardamom , and cloves , garnished with fried onions 
+'''
+# these 3 quoation is for paragraph 
+
+print(recipi)  #veg biryani with saffron , cardamom , and cloves , garnished with fried onions 
+
+spice = "cardamom"
+spice in recipi # true
+
+"milk" in recipi # false 
+
+print(recipi.replace("veg","non-veg") ) # it will print , but it will not change the orignal string 
+nw = recipi.replace("veg","non-veg") # replace funcion will give a entire new string and changes happen in new striing 
+
+recipi.upper() # simliraly lower s also there
+
+recipi.index("garnished") # this will give the starting index of given word or striig
+
+text = "my age is :"
+age = 38
+
+text + age  # this will give a error ,  so convert int into string '38' or str(age)
+text + str(age) # now it will work 
+
+
+tickers="AAPL|NVDIA|RIL|GOGL"
+tickers.split("|")  # ['AAPL', 'NVDIA', 'RIL' ,'GOGL']
+tickers.split("|" , maxsplit = 2) #['AAPL', 'NVDIA', 'RIL|GOGL']
+
+data = " helloo we are doing the same thing again and again "
+data.strip() # it removes the extra spaces and trailing spaces from your body 
+
+file_name = "report.pdf"
+
+file_name.endswith(".pdf")  # usally these are used in checking of the format of file 
+
+# Create a variable and store the string “The Himalayas are one of the youngest mountain ranges on the planet.”
+
+# 01 Print ‘The Himalayas’ using slice operator
+# 02 Print “mountain range” using a negative index
+# 03 Print “The Himalayas on the planet” using slice as well as f-string
+
+st = "The Himalayas are one of the youngest mountain ranges on the planet"
+
+print(st[0:13])  # 13 is not included 
+print(st[-29:-14])
+print(st[0:13],st[54:67]) 
+print(st[0:13],st[-13:])
+#outputs
+# The Himalayas
+# mountain ranges
+# The Himalayas on the planet
+# The Himalayas on the planet
+------------------------------
+# Q2
+# You have created a string variable called string= ”There are 9 planets in the solar system”.
+# After some time, you have realized that your sentence is incorrect and there are only 8 planets, 
+# now correct your sentence by replacing the incorrect words. (Bonus: Try to do it in one line)
+
+st = "There are 9 planets in the solar system"
+nw = st.replace("9","8")
+nw
+
+# output
+# There are 8 planets in the solar system'
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
